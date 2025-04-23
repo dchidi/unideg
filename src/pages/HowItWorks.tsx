9227a2ae444f4566c8796db851ac8299bad9f97c
Chidi Duru <tchidi4real@gmail.com>
2025-04-23 17:25:11 +0100

    Implement timeline

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -1,6 +1,22 @@
 import React from "react";
+import TimeLine from "../components/ui/TimeLine";
 
 const HowItWorks: React.FC = () => {
-  return <>how it works</>;
+  return (
+    <div style={{ padding: "2rem" }}>
+      <TimeLine
+        status="pending"
+        title="Created application"
+        description="Somethings goes in here. And other things"
+        date="04/12/2025"
+      />
+      <TimeLine
+        status="pending"
+        title="Created application"
+        description="Somethings goes in here. And other things"
+        date=""
+      />
+    </div>
+  );
 };
 export default HowItWorks;
